e2f61be51b6eabd1039dcabefbd839eb0ef87e43
0x59R11 <41921371+0x59R11@users.noreply.github.com>
2023-03-04 20:49:23 +0200

    Init commit

---
+++ DarkClearInventory/ClearInventory.cs
@@ -0,0 +1,112 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+
+namespace DarkClearInventory
+{
+    public class ClearInventory : RocketPlugin
+    {
+        public static ClearInventory Instance = null;
+
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+        protected override void Unload()
+        {
+            
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "clear_successfully", "У {0} инвентарь успешно очищен" },
+            { "clear_failed", "Что-то пошло не так. (проверьте консоль)" },
+            { "clear_not_have_permission_other", "У вас не достаточно прав." },
+            { "clear_invalid_command", "{0}" }
+        };
+
+
+        public bool ClearPlayerInventory(UnturnedPlayer player)
+        {
+            return ClearItems(player) && ClearClothes(player);
+        }
+
+
+        public bool ClearItems(UnturnedPlayer player)
+        {
+            bool result = false;
+            try
+            {
+                player.Player.equipment.dequip();
+                for (byte page = 0; page < PlayerInventory.PAGES - 1; page++)
+                {
+                    byte itemCount = player.Player.inventory.getItemCount(page);
+                    if (itemCount > 0)
+                    {
+                        for (byte i = 0; i < itemCount; i++)
+                        {
+                            player.Player.inventory.removeItem(page, 0);
+                        }
+                    }
+                }
+                player.Player.channel.send("tellSlot", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[] { 0, 0, new byte[0] });
+                player.Player.channel.send("tellSlot", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[] { 1, 0, new byte[0] });
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Rocket.Core.Logging.Logger.Log($"There was an error clearing {player.CharacterName}'s inventory.  Here is the error.", ConsoleColor.White);
+                Console.Write(ex);
+            }
+            return result;
+        }
+
+        public bool ClearClothes(UnturnedPlayer player)
+        {
+            bool result = false;
+            try
+            {
+                player.Player.clothing.askWearBackpack(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearGlasses(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearHat(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearMask(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearPants(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearShirt(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                player.Player.clothing.askWearVest(0, 0, new byte[0], true);
+                removeCloth(player.Player.inventory);
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Rocket.Core.Logging.Logger.Log($"There was an error clearing {player.CharacterName}'s inventory.  Here is the error.", ConsoleColor.White);
+                Console.Write(ex);
+            }
+            return result;
+        }
+
+
+        private void removeCloth(PlayerInventory inventory)
+        {
+            for (byte b = 0; b < inventory.getItemCount(2); b++)
+            {
+                inventory.removeItem(2, 0);
+            }
+        }
+    }
+}
---
+++ DarkClearInventory/CommandCi.cs
@@ -0,0 +1,48 @@
+﻿using Rocket.API;
+using Rocket.Core;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DarkClearInventory
+{
+    public class CommandCi : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "ci";
+        public string Help => "";
+        public string Syntax => "/ci [name/self]";
+        public List<string> Aliases => new List<string>() { "clearinventory", "cleari" };
+        public List<string> Permissions => new List<string>() { "ci.other", "ci" };
+
+        
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            if (command.Length > 1)
+            {
+                UnturnedChat.Say(player, ClearInventory.Instance.Translate("clear_invalid_command", Syntax), Color.yellow);
+                return;
+            }
+            UnturnedPlayer toPlayer = player;
+            if (command.Length == 1 && command[0].ToLower() != "self")
+            {
+                if (!R.Permissions.HasPermission(player, "ci.other") && !player.IsAdmin)
+                {
+                    UnturnedChat.Say(player, ClearInventory.Instance.Translate("clear_not_have_permission_other"), Color.red);
+                    return;
+                }
+                toPlayer = UnturnedPlayer.FromName(command[0]);
+            }
+            bool cleared = ClearInventory.Instance.ClearPlayerInventory(toPlayer);
+            if (!cleared)
+            {
+                UnturnedChat.Say(player, ClearInventory.Instance.Translate("clear_failed", toPlayer.CharacterName), Color.red);
+                return;
+            }
+            UnturnedChat.Say(player, ClearInventory.Instance.Translate("clear_successfully", toPlayer.CharacterName), Color.green);
+        }
+    }
+}
